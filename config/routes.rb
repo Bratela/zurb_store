a4a43c69587273f4ee50c7495ea009d47fc9cd1c
Bratela <den.mitrofanov@gmail.com>
2015-07-26 22:27:28 +0300

    menu bar nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/price'
+
+  get 'pages/contacts'
+
   resources :products
   root 'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
